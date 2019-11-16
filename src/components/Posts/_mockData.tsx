81af9a07b66bb98c8013e17102aeabafe9e99e22
dua <paxxan98@gmail.com>
2019-11-16 20:39:15 +0500

    started mock and..

---
+++ src/components/Posts/_mockData.tsx
@@ -0,0 +1,42 @@
+
+// `
+// query GetDrafts {
+//   drafts {
+//     id
+//     description
+//     type
+//     attachments {
+//       id
+//       path
+//       mime
+//       source
+//       thumbnail
+//       size
+//       height
+//       width
+//       status
+//     }
+//     owner {
+//       id
+//       name
+//       avatar
+//     }
+//     category {
+//       id
+//       name
+//     }
+//     created_at
+//     updated_at
+//     when
+//   }
+// }
+// `;
+
+export const draft = {
+  id: 'dsa',
+  description: 'dasklfjl fwklfw eklfjwe fewklkl frwelkrj jfk dlskjsd ljdsf',
+  type: 'image',
+  attachments: {
+    // fu,,
+  }
+}
